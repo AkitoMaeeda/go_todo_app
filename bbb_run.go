9443e659119c9363844ce9b8164fb605309dac9c
shinjyaga <shinjyaga@DESKTOP-PJ4TR9U.localdomain>
2023-03-17 15:05:54 +0900

    3/17

--- bbb_run.go
+++ bbb_run.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/AkitoMaeeda/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
@@ -13,20 +16,26 @@ import (
 
 func run(ctx context.Context) error {
 
+	ctx, stop := signal.NotifyContext(ctx, os.Interttupt, syscall.SIGTERM)
+	defar stop()
+
 	cfg, err := config.New()
 	if err != nil {
 		return err
 	}
 
+	log.Printf("利用するポート :%d\n", cfg.Port)
+
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
-		log.Fatalf("failed to kisten port %d: %v", cfg.Port, err)
+		log.Fatalf("failed to kisten port %d: \n%v", cfg.Port, err)
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
@@ -44,6 +53,7 @@ func run(ctx context.Context) error {
 
 	<-ctx.Done()
 	if err := s.Shutdown(context.Background()); err != nil {
+
 		log.Printf("failed to shutdown: %+v", err)
 	}
 
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
-module github.com/budougumi0617/go_todo_app
-
-go 1.18
+module github.com/AkitoMaeeda/go_todo_app
 
 require golang.org/x/sync v0.1.0
 
 require github.com/caarlos0/env/v6 v6.10.1 // indirect
+
+go 1.18
